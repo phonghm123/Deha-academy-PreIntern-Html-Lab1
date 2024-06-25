d322a167f1e1f78508cd255034fed6a17bb494fb
Phong Nguyen <phonghm193@gmail.com>
2024-06-25 09:03:22 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>test</title>
+        <style>
+            .h3{
+                opacity: 0,1;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1>learn more about beautiful furnite web design combinations</h1>
+            <img src="" alt="img">
+            <h2>Today we will teach you how to find professional materials for your furnite website design.</h2>
+            <h3>abccccccccccccccccccccccccc</h3>
+        </div>
+    </body>
+</html>
